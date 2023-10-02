9bb0b2e417f8ecaab2f9e29264f264663acac67a
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-10-02 13:38:20 -0300

    fix: project

---
+++ NewTalents/TestNewTalents/CalculadoraTests.cs
@@ -0,0 +1,71 @@
+using NewTalents;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace TestNewTalents
+{
+    public class CalculadoraTests
+    {
+        private Calculadora _calc;
+
+        public CalculadoraTests()
+        {
+            _calc = new Calculadora();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 2, 2 }, 4)]
+        [InlineData(new int[] { 3, 3 }, 6)]
+        public void SomarTeste(int[] num, int resultado)
+        {
+            int soma = _calc.Somar(num[0], num[1]);
+            Assert.Equal(resultado, soma);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 2, 2 }, 0)]
+        [InlineData(new int[] { 3, 2 }, 1)]
+        public void SubtrairTeste(int[] num, int resultado)
+        {
+            int subtrair = _calc.Subtrair(num[0], num[1]);
+            Assert.Equal(resultado, subtrair);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 2, 2 }, 4)]
+        [InlineData(new int[] { 3, 2 }, 6)]
+        public void MultiplicarTeste(int[] num, int resultado)
+        {
+            int multiplicar = _calc.Multiplicar(num[0], num[1]);
+            Assert.Equal(resultado, multiplicar);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 10, 2 }, 5)]
+        [InlineData(new int[] { 4, 2 }, 2)]
+        public void DivisaoTeste(int[] num, int resultado)
+        {
+            int divisao = _calc.Dividir(num[0], num[1]);
+            Assert.Equal(resultado, divisao);
+        }
+
+        [Fact]
+        public void DivisaoPorZero()
+        {
+            Assert.Throws<DivideByZeroException>(
+                () => _calc.Dividir(2, 0)
+            );
+        }
+
+
+        [Fact]
+        public void HistoricoTeste()
+        {
+            List<string> historico = _calc.Historico();
+
+            Assert.Equal(3, historico.Count);
+
+        }
+    }
+}
